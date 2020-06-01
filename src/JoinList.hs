4378c6d164cf843a3d5d712a7377090ac2ec1706
Anay Nayak <anayak007@gmail.com>
2020-06-01 15:20:37 +0530

    week 7.1

--- src/JoinList.hs
+++ src/JoinList.hs
@@ -1,1 +1,30 @@
+{-# LANGUAGE FlexibleInstances #-}
 module JoinList where
+import Sized
+
+data JoinList m a = Empty
+                | Single m a
+                | Append m (JoinList m a) (JoinList m a)
+                deriving (Eq, Show)
+
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) = mappend 
+
+instance Monoid m => Monoid (JoinList m a) where
+    mempty = Empty
+
+instance Monoid m => Semigroup (JoinList m a) where
+    (<>) jl1 jl2 = 
+        let
+            m1 = tag jl1
+            m2 = tag jl2
+        in Append (m1 <> m2) jl1 jl2
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ n jl = undefined
