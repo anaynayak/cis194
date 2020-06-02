a0d83642fedbdd5e880181c1d17a842d39ca26eb
Anay Nayak <anayak007@gmail.com>
2020-06-02 16:21:26 +0530

    week 8

---
+++ src/Party.hs
@@ -0,0 +1,34 @@
+module Party where
+import Employee
+import Data.Tree 
+import Data.List 
+
+instance Semigroup GuestList where
+    (<>) (GL g1 f1) (GL g2 f2) = GL (g1 ++ g2) (f1 + f2)
+
+instance Monoid GuestList where
+    mempty = GL [] 0
+
+glCons :: Employee -> GuestList -> GuestList
+glCons e (GL g f)= GL (e:g) (f + empFun e)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun = max
+
+treeFold ::  (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node a fs) = f a $ map (treeFold f) fs
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e gs = (glCons e withBosses, moreFun withBosses withoutBosses) where
+    withBosses =  mconcat (map snd gs)
+    withoutBosses = mconcat $ map fst gs
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel 
+
+
+main :: IO ()
+main = readFile "company.txt" >>= (putStr . print . maxFun . read ) where
+    print (GL es f) = (unlines . summary . sort . map empName) es
+        where summary = (++) ["Total fun: "  ++ (show f)]
+    
