2539f760a1c84601b466ab55c4eef8d86d15657b
Anay Nayak <anayak007@gmail.com>
2020-06-05 22:31:24 +0530

    week 11 files

--- src/AParser.hs
+++ src/AParser.hs
@@ -1,51 +1,21 @@
-{- CIS 194 HW 10
-   due Monday, 1 April
--}
-
-module AParser where
+module AParser (Parser, runParser, satisfy, char, posInt) where
 
 import           Control.Applicative
-
 import           Data.Char
 
--- A parser for a value of type a is a function which takes a String
--- represnting the input to be parsed, and succeeds or fails; if it
--- succeeds, it returns the parsed value along with the remainder of
--- the input.
 newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 
--- For example, 'satisfy' takes a predicate on Char, and constructs a
--- parser which succeeds only if it sees a Char that satisfies the
--- predicate (which it then returns).  If it encounters a Char that
--- does not satisfy the predicate (or an empty input), it fails.
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
   where
-    f [] = Nothing    -- fail on the empty input
-    f (x:xs)          -- check if x satisfies the predicate
-                        -- if so, return x along with the remainder
-                        -- of the input (that is, xs)
+    f [] = Nothing
+    f (x:xs)
         | p x       = Just (x, xs)
-        | otherwise = Nothing  -- otherwise, fail
+        | otherwise = Nothing
 
--- Using satisfy, we can define the parser 'char c' which expects to
--- see exactly the character c, and fails otherwise.
 char :: Char -> Parser Char
 char c = satisfy (== c)
 
-{- For example:
-
-*Parser> runParser (satisfy isUpper) "ABC"
-Just ('A',"BC")
-*Parser> runParser (satisfy isUpper) "abc"
-Nothing
-*Parser> runParser (char 'x') "xyz"
-Just ('x',"yz")
-
--}
-
--- For convenience, we've also provided a parser for positive
--- integers.
 posInt :: Parser Integer
 posInt = Parser f
   where
@@ -54,39 +24,21 @@ posInt = Parser f
       | otherwise = Just (read ns, rest)
       where (ns, rest) = span isDigit xs
 
-------------------------------------------------------------
--- Your code goes below here
-------------------------------------------------------------
+inParser f = Parser . f . runParser
 
-first :: (a -> b) -> (a, c) -> (b, c)
-first f (a, c) = (f a, c)
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (x,y) = (f x, y)
 
 instance Functor Parser where
-  fmap f (Parser a) = Parser (fmap (first f) . a)
+  fmap = inParser . fmap . fmap . first
 
 instance Applicative Parser where
-  pure a = Parser (\s -> Just (a , s))
-  (<*>) pa2b pa = Parser fn where
-    fn s = case runParser pa2b s of
-      Nothing -> Nothing
-      Just (f, s2) -> runParser (f <$> pa) s2
-
-abParser :: Parser (Char, Char)
-abParser = pure (,) <*> char 'a' <*> char 'b'
-
-abParser_ :: Parser ()
-abParser_ = pure (const ()) <*> abParser
-
-intPair :: Parser [Integer]
-intPair = pure (\a b c -> a:[c]) <*> posInt <*> char ' ' <*> posInt
+  pure a = Parser (\s -> Just (a, s))
+  (Parser fp) <*> xp = Parser $ \s ->
+    case fp s of
+      Nothing     -> Nothing
+      Just (f,s') -> runParser (f <$> xp) s'
 
 instance Alternative Parser where
-  empty = Parser $ const Nothing
-  (<|>) p1 p2 = Parser fn where
-    fn s = case runParser p1 s of
-      Nothing -> runParser p2 s
-      j -> j
-
-intOrUppercase :: Parser ()
-intOrUppercase = (noreturn <*> posInt) <|> (noreturn <*> (satisfy isUpper)) where
-  noreturn = pure (const ())
+  empty = Parser (const Nothing)
+  Parser p1 <|> Parser p2 = Parser $ liftA2 (<|>) p1 p2
